a1dab3d84bc21b058b53e30b0e0bc357b4bd056d
Er0r <47925596+Er0r@users.noreply.github.com>
2021-05-12 01:40:46 +0600

    Create App.js

---
+++ React/HOC/App.js
@@ -0,0 +1,14 @@
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+import Grocery from './components/Grocery';
+function App() {
+  return(
+  <div> 
+    <ClickCounter />
+    <HoverCounter />
+    <Grocery />
+  </div>
+  )
+}
+
+export default App;
